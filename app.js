31b14d632e46171e18e592103f68d978fd221681
yenny <yenny_manganti@yahoo.com>
2022-11-10 16:37:03 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const https = require("https");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//get request
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/Signup.html");
+});
+
+// post request
+app.post("/", (req, res) => {
+  const FirstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+  console.log(FirstName, lastName, email);
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: FirstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  //data saved in constant jasonData
+  const jsonData = JSON.stringify(data);
+
+  // mailchimp url
+  const url = "https://us21.api.mailchimp.com/3.0/lists/fd8e286e5f";
+
+  // options
+  const option = {
+    method: "POST",
+    auth: "jas02:15edfc0b1180b813f684d37b0e986b72-us21",
+  };
+
+  // https request
+  const request = https.request(url, option, (response) => {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", (data) => {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+// post request to failure route
+app.post("/failure", (req, res) => {
+  res.redirect("/");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("Server is running on Port 3000");
+});
